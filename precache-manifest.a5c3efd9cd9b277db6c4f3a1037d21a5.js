5b057122f198f41f36c4e1649b70efb20a6ec9bc
胡本绿 <benlv.hu@dianrong.com>
2019-09-04 17:52:45 +0800

    Updates

---
+++ precache-manifest.a5c3efd9cd9b277db6c4f3a1037d21a5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "2786c7710a406833f9c0",
+    "url": "/benlv/static/js/runtime~resources.a3d0bab9.2786c771.js"
+  },
+  {
+    "revision": "e5bf3c335e834e1b5567",
+    "url": "/benlv/static/js/runtime~index.b05201a7.e5bf3c33.js"
+  },
+  {
+    "revision": "45de2b7f44aefa774f0c",
+    "url": "/benlv/static/js/resources.a3d0bab9.45de2b7f.chunk.js"
+  },
+  {
+    "revision": "9e7d46a30e9d4acc0484",
+    "url": "/benlv/static/js/index.b05201a7.9e7d46a3.chunk.js"
+  },
+  {
+    "revision": "dacb55d0c473049add4e",
+    "url": "/benlv/static/js/3.dacb55d0.chunk.js"
+  },
+  {
+    "revision": "de8be4a5735b1e2baf17",
+    "url": "/benlv/static/js/0.de8be4a5.chunk.js"
+  },
+  {
+    "revision": "45de2b7f44aefa774f0c",
+    "url": "/benlv/static/css/resources.a3d0bab9.53bc50fb.chunk.css"
+  },
+  {
+    "revision": "9e7d46a30e9d4acc0484",
+    "url": "/benlv/static/css/index.b05201a7.f4be8408.chunk.css"
+  },
+  {
+    "revision": "dacb55d0c473049add4e",
+    "url": "/benlv/static/css/3.dc615ba3.chunk.css"
+  },
+  {
+    "revision": "7ca8f564936ad79fd9e9e09a878b0110",
+    "url": "/benlv/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b85f809636ca0672a8f3db4bec34d0a4.js"
+  "/benlv/precache-manifest.a5c3efd9cd9b277db6c4f3a1037d21a5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/benlv/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
